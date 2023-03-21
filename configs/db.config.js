5e177566e81bf6d896f4299c6a03e5d8bc4fa375
Vishal Rathod <73741136+vishalok@users.noreply.github.com>
2023-03-21 13:11:05 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_Name:"CRM_DB",
-    DB_URL:"mongodb+srv://vishalrathod:3xuftRGoAG6Omicv@cluster0.fkzfmdc.mongodb.net/?retryWrites=true&w=majority"
+    DB_URL:"mongodb+srv://vishalrathod:3xuftRGoAG6Omicv1@cluster0.fkzfmdc.mongodb.net/?retryWrites=true&w=majority"
 }
